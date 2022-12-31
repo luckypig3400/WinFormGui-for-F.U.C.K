ca96a251255748a4dda96a71e888b206f5cd61c3
郵差 <twHenryYang@gmail.com>
2022-12-31 14:10:49 +0800

    refactor: Project Assembly info

--- WinFormGui-for-F.U.C.K/Properties/AssemblyInfo.cs
+++ WinFormGui-for-F.U.C.K/Properties/AssemblyInfo.cs
@@ -2,35 +2,36 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的常规信息通过以下
-// 特性集控制。更改这些特性值可修改
-// 与程序集关联的信息。
-[assembly: AssemblyTitle("HairSalonMS")]
+// 有關程式集的常規資訊通過以下
+// 特性集控制。更改這些特性值可修改
+// 與程式集關聯的資訊。
+[assembly: AssemblyTitle("WinFormGui-for-F.U.C.K")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HairSalonMS")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("WinFormGui-for-F.U.C.K")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// 将 ComVisible 设置为 false 使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
-// 则将该类型上的 ComVisible 特性设置为 true。
+// 將 ComVisible 設置為 false 使此程式集中的類型
+// 對 COM 組件不可見。如果需要從 COM 訪問此程式集中的類型，
+// 則將該類型上的 ComVisible 特性設置為 true。
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+// 如果此項目向 COM 公開，則下列 GUID 用於型別程式庫的 ID
 [assembly: Guid("c2cf9f59-0a1f-4d81-a15a-9636db62b7eb")]
 
-// 程序集的版本信息由下面四个值组成:
+// 程式集的版本資訊由下面四個值組成:
 //
 //      主版本
 //      次版本 
-//      内部版本号
-//      修订号
+//      內部版本號
+//      修訂號
 //
-// 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
+// 可以指定所有這些值，也可以使用“內部版本號”和“修訂號”的預設值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
