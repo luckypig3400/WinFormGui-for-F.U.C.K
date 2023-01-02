e4d097c03710c97d90ad7e2f9831978b1235ed12
郵差 <twHenryYang@gmail.com>
2023-01-02 09:45:27 +0800

    style: Adjust Home page font size

--- WinFormGui-for-F.U.C.K/childForm/childForm_home.Designer.cs
+++ WinFormGui-for-F.U.C.K/childForm/childForm_home.Designer.cs
@@ -47,11 +47,11 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(132, 465);
+            this.label1.Location = new System.Drawing.Point(104, 459);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(797, 128);
+            this.label1.Size = new System.Drawing.Size(853, 138);
             this.label1.TabIndex = 1;
             this.label1.Text = "C# Windows Form GUI for \r\nFHIR-Universal-Conversion-Kit";
             // 
@@ -79,7 +79,7 @@
             this.label2.ForeColor = System.Drawing.Color.Black;
             this.label2.Location = new System.Drawing.Point(402, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 64);
+            this.label2.Size = new System.Drawing.Size(75, 79);
             this.label2.TabIndex = 5;
             this.label2.Text = "+";
             // 
@@ -139,7 +139,7 @@
             this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(684, 61);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 64);
+            this.label3.Size = new System.Drawing.Size(75, 79);
             this.label3.TabIndex = 9;
             this.label3.Text = "+";
             // 
@@ -151,7 +151,7 @@
             this.label4.ForeColor = System.Drawing.Color.Black;
             this.label4.Location = new System.Drawing.Point(201, 285);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 64);
+            this.label4.Size = new System.Drawing.Size(75, 79);
             this.label4.TabIndex = 10;
             this.label4.Text = "=";
             // 
