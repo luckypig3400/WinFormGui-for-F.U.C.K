5cfc9e93e9f70f87ded5e24533083e10a98fa6c0
郵差 <twHenryYang@gmail.com>
2023-01-02 16:40:10 +0800

    misc: remove debug info

--- WinFormGui-for-F.U.C.K/childForm/childForm_home.cs
+++ WinFormGui-for-F.U.C.K/childForm/childForm_home.cs
@@ -18,7 +18,6 @@ namespace WFGF.U.C.K.childForm
 
         private void btn_readManual_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("測試是否順利覆蓋舊版本的檔案");
             if(DialogResult.Yes == MessageBox.Show("教學手冊為線上HackMD文件，連結如下:\nhttps://hackmd.io/@LuckyPig/WinFormGui-for-FUCK_manual\n是否於瀏覽器中打開?",
                 "打開使用手冊?", MessageBoxButtons.YesNo, MessageBoxIcon.Information))
             {// https://stackoverflow.com/questions/14819426/how-to-create-hyperlink-in-messagebox-show
